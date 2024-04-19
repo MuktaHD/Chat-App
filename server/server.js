917d6e68da9acb604a1d74fc9a37872ca5bf5a43
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-04-19 16:55:39 +0530

    Update server.js
    add the first line comments how to install equire

--- server/server.js
+++ server/server.js
@@ -1,36 +1,9 @@
-// const http =require('http');
-// const path=require('path');
-// const express =require('express');
-// const socketIO=require('socket.io');
 
-// const app=express();
-
-// const server=http.createServer(app);
-
-// const io=socketIO(server);
-// //Server static files from the public directory
-// app.use(express.static(path.join(__dirname,'../public')));
-
-// //listen for a connection to the websocket
-
-// io.on('connection',function(socket){
-//     console.log('a user connected');
-//     //listen for chat messages
-//     socket.on('chat message',function(data){
-//         io.emit('chat message',data);
-//     });
-//     //listen for a disconnect
-//     socket.on('disconnect',()=>{
-//         console.log('user disconnected');
-//     });
-// });
-// //start the server
-// const PORT=process.env.PORT || 3003;
-
-// server.listen(PORT,function(){
-//     console.log('listening on port ${PORT} ');
-// });
-
+//first install the require using terminal type 
+//npm i http
+//npm i path
+//npm i express
+//nmp i socket.io
 
 const http=require('http');
 const path=require('path');
