96b4d804c08306ce10e9081b030e000057a3df5e
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-04-23 17:11:08 +0530

    Update server.js
    add app.use(express.static(path.join(__dirname, '../public')));

--- server/server.js
+++ server/server.js
@@ -1,36 +1,9 @@
-// const http =require('http');
-// const path=require('path');
-// const express =require('express');
-// const socketIO=require('socket.io');
 
-// const app=express();
-
-// const server=http.createServer(app);
-
-// const io=socketIO(server);
-// //Server static files from the public directory
-// app.use(express.static(path.join(__dirname,'../public')));
-
-// //listen for a connection to the websocket
-
-// io.on('connection',function(socket){
-//     console.log('a user connected');
-//     //listen for chat messages
-//     socket.on('chat message',function(data){
-//         io.emit('chat message',data);
-//     });
-//     //listen for a disconnect
-//     socket.on('disconnect',()=>{
-//         console.log('user disconnected');
-//     });
-// });
-// //start the server
-// const PORT=process.env.PORT || 3003;
-
-// server.listen(PORT,function(){
-//     console.log('listening on port ${PORT} ');
-// });
-
+//first install the require using terminal type 
+//npm i http
+//npm i path
+//npm i express
+//nmp i socket.io
 
 const http=require('http');
 const path=require('path');
@@ -39,8 +12,8 @@ const socketIO=require('socket.io');
 const app=express();
 const server=http.createServer(app);
 const io=socketIO(server);
-
-app.use(express.static('../public'));
+app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static('../public'));
 io.on('connection',function(socket)
 {
     console.log('A user connected');
@@ -55,5 +28,5 @@ io.on('connection',function(socket)
 });
 const PORT=process.env.PORT||3004;
     server.listen(PORT,function(){
-        console.log(`server is running on port ${PORT}`);
+        console.log(`server is running on port http://localhost:${PORT}`);
     })
